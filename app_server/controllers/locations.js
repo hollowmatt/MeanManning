eaf8c9df92d255eb445dd8707fac47bfc9c90451
Matt Holloway <hollowmatt@gmail.com>
2024-09-15 17:31:24 -0400

    complete refactoring of locations-list page

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -8,6 +8,9 @@ const homeList = (req, res) => {
       title: 'Loc8r',
       strapLine: 'Find places to work with wifi near you'
     },
+    sidebar: "Looking for wifi and a seat? Loc8r helps you find places to " + 
+      "work when out and about. Perhaps with coffee, cake or a pint? " +
+      "Let Loc8r help you find the place you're looking for.",
     locations: [{
       name: 'Starcups',
       address: '125 High Street, Reading, Rg6 1PS',
