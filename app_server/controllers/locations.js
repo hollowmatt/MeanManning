0a8d18682bbfdf1ee62529300771693180205ed5
Matt Holloway <hollowmatt@gmail.com>
2024-09-15 13:28:41 -0400

    add .env for environment variables to store map API

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,9 +1,16 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const API = process.env.API;
 const homeList = (req, res) => {
   res.render('locations-list', {title: 'Home Page' });
 };
 
 const locationInfo = (req, res) => {
-  res.render('index', {title: 'Detail Page' });
+  res.render('location-info', 
+    {
+      title: 'Detail Page', 
+      mapurl:`https://maps.googleapis.com/maps/api/staticmap?center=51.455041,-0.9690884&zoom=17&size=400x350&sensor=false&markers=51.455041,-0.9690884&scale=2&key=${API}`
+    });
 };
 
 const addReview = (req, res) => {
