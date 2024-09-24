f8caaf2f32a6dfde64f28afc5332ffde59f42f2e
Matt Holloway <hollowmatt@gmail.com>
2024-09-24 19:32:25 -0400

    finish up the database schema

--- app_server/models/locations.js
+++ app_server/models/locations.js
@@ -1,5 +1,33 @@
 const mongoose = require('mongoose');
 
+const openingTimeSchema = new mongoose.Schema({
+  days: {
+    type: String,
+    required: true
+  },
+  opening: String,
+  closing: String,
+  closed: {
+    type: Boolean,
+    required: true
+  }
+});
+
+const reviewSchema = new mongoose.Schema({
+  author: String,
+  rating: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 5
+  },
+  reviewText: String,
+  createdOn: {
+    type: Date,
+    'default': Date.now
+  }
+})
+
 const locationSchema = new mongoose.Schema({
   name: {
     type: String, 
@@ -10,5 +38,15 @@ const locationSchema = new mongoose.Schema({
     default: 0, 
     min: 0, 
     max: 5 },
-  facilities: [String]
+  facilities: [String],
+  coords: {
+    type: {
+      type: string
+    },
+    coordinates: [number]
+  },
+  openTimes: [openingTimeSchema],
+  reviews: [reviewSchema]
 });
+
+locationSchema.index({coords: '2dsphere'});
