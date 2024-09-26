f3b4115604a243e296c36692cb8a30f6a5223623
Matt Holloway <hollowmatt@gmail.com>
2024-09-26 20:15:07 +0100

    stub out remaining locations apis

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -1,16 +1,31 @@
+const mongoose = require('mongoose');
+const Loc = mongoose.model('Location');
+
 const locationsListByDistance = (req, res) => {
   res
     .status(200)
-    .json({"location":"stub"});
+    .json({"status":"locationsListByDistance: get success"});
 };
 const locationsCreate = (req, res) => {
   res
     .status(200)
-    .json({"status": "success"});
+    .json({"status": "locationsCreate: post success"});
 };
-const locationsReadOne = (req, res) => {};
-const locationsUpdateOne = (req, res) => {};
-const locationsDeleteOne = (req, res) => {};
+const locationsReadOne = (req, res) => {
+  res
+    .status(200)
+    .json({"status": "locationsReadOne: get success"});
+};
+const locationsUpdateOne = (req, res) => {
+  res
+    .status(200)
+    .json({"status": "locationsUpdateOne: put success"});
+};
+const locationsDeleteOne = (req, res) => {
+  res
+    .status(200)
+    .json({"status": "locationsDeleteOne: delete success"});
+};
 
 module.exports = {
   locationsListByDistance,
