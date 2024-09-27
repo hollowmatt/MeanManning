2b3b298f3db62e5295861eef07193f66fd5ad5e7
Matt Holloway <hollowmatt@gmail.com>
2024-09-27 18:07:55 +0100

    update locationsReadOne

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -11,10 +11,24 @@ const locationsCreate = (req, res) => {
     .status(200)
     .json({"status": "locationsCreate: post success"});
 };
-const locationsReadOne = (req, res) => {
-  res
-    .status(200)
-    .json({"status": "locationsReadOne: get success"});
+const locationsReadOne = (req, res) => { 
+  Loc
+    .findById(req.params.locationId)
+    .exec((err, location) => {
+      if (!location) {
+        return res
+          .status(404)
+          .json({"message": "location not found"});
+      } else if (err) {
+        return res
+          .status(404)
+          .json(err);
+      } else {
+        return res
+          .status(200)
+          .json(location);
+      }
+    });
 };
 const locationsUpdateOne = (req, res) => {
   res
