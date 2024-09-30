3d5e1335dfb731ecd303c105912c70e7e2b2452a
Matt Holloway <hollowmatt@gmail.com>
2024-09-30 21:59:04 +0100

    initial version of add a location

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -9,7 +9,47 @@ async function locationsListByDistance(req, res) {
     .status(200)
     .json(locations);
 };
-const locationsCreate = (req, res) => {
+
+async function locationsCreate(req, res) {
+  try {
+    const location = await Loc.create({
+      name: req.body.name,
+      address: req.body.address,
+      facilities: req.body.facilities.split(","),
+      coords: {
+        type: "Point",
+        coordinates: [
+          parseFloat(req.body.lng),
+          parseFloat(req.body.lat)
+        ]
+      },
+      openingTimes: [
+        {
+          days: req.body.days1,
+          opening: req.body.opening1,
+          closing: req.body.closing1,
+          closed: req.body.closed1
+        },
+        {
+          days: req.body.days2,
+          opening: req.body.opening2,
+          closing: req.body.closing2,
+          closed: req.body.closed2
+        }
+      ]
+    }) 
+    if (!location) {
+      return res
+        .status(404)
+        .json({"message": "location not saved"});
+    } else {
+      return res
+        .status(200)
+        .json({"message": "saved"});
+    }
+  } catch (err) {
+    return res.status(400).json({"message": "invalid input", "error": err});
+  }
   res
     .status(200)
     .json({"status": "locationsCreate: post success"});
